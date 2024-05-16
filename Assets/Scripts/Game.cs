6b4edeea21371429604f64402a0da117cfc237f7
Lucas Lazaroiu <73948966+luksy26@users.noreply.github.com>
2024-05-16 22:24:12 +0300

    First Controller to generate piece

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{   
+    public GameObject chessPiece;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(chessPiece, new Vector3(0, 0, -1), Quaternion.identity);
+    }
+}
