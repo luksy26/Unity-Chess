b00e7a87b1ea11575a4d9db96997586ee150207e
Lucas Lazaroiu <73948966+luksy26@users.noreply.github.com>
2024-07-01 23:07:41 +0300

    Dev Corner

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void QuitGame() {
         Application.Quit();
+    }
+    public void LoadDevCorner() {
+        SelectedOption.AI = -1;
+        SelectedOption.tutorial = -1;
+        SceneManager.LoadScene("BoardScene");
     }
 }
